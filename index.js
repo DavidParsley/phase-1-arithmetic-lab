4affa04f48ee24755ec7dcfe94af91bcf14255a0
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-10-30 20:26:26 +0300

    Transferred from main account

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 // Write your code here
 const num1 = 31; 
 const num2 = 2; 
-const multiply = num1 * num2; 
+const multiply = num1 * num2 
+
+
 
 const random = Math.floor(Math.random() * 100) + 1;
 
